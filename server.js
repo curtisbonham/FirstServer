be6d1c7d5b4faffcf2f444c6e1761186b35c02ae
curtisbonham <curtis.bonham1@gmail.com>
2025-03-10 09:32:03 -0600

    made it 10 lines or less

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 import * as http from 'node:http'
-
 const server = http.createServer((req, res) => {
   let url = req.url.split('/');
   if(url[1] === '') {
-    if(req.method === 'GET') {
-      res.end('Hello, Galvanize!');
-    }
-  }
+    if(req.method === 'GET') {res.end('Hello, Galvanize!');}}
   res.statusCode = 404;
-  res.end();
-});
-
+  res.end();});
 server.listen(3000, "localhost", () => {
-  console.log(`Server running at http://localhost:3000/`);
-});
+  console.log(`Server running at http://localhost:3000/`);});
